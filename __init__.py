b2f9217e2552a0be67fdf24a17236e7bfe154187
Jerryning <ningjun24@163.com>
2018-09-13 16:22:37 +0800

    Revert "cookies login"
    This reverts commit 6fc6cb4d5641ba1c39f235100d5cf82aa6d39749.

--- __init__.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-from Xin_bang import xin_bang
-
-if __name__ == '__main__':
-    pass
--- utils/settings.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-# ES 配置
-
-import os
-PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))  # 项目目录
